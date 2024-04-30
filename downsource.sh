111109582a5c5dfd332a64603ae6cdd1a01e6941
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-04-30 15:37:44 -0600

    downsource.sh

---
+++ downsource.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the GitHub repository
+git clone https://github.com/RaglemL/source.git
+
+# Change directory to the repository
+cd source
+
+# Change permissions of target.sh
+chmod +x source.sh
+
+# Execute target.sh
+./source.sh
